183e3dbc0c77fb17f8ee53337c00e0e5e1fea30b
Kiersten White <kierstenswhite@gmail.com>
2023-02-28 10:49:21 -0500

    Hurricane Ironstorm Boilerplate

---
+++ scripts/mexico/main.js
@@ -0,0 +1,52 @@
+const country /*(jamaica, japan, mexico, newZealand)*/ = {
+
+    countryImage: ,
+    city: [
+        {
+            name: -,
+            population: -,
+            landmark: -,
+            landmarkImage:-,
+        },
+        {
+    
+            name: -,
+            population: -,
+            landmark: -,
+            landmarkImage:-,
+        },
+        {
+            name: -,
+            population: -,
+            landmark: -,
+            landmarkImage:-,
+    
+        },
+    ],
+    
+    celebrity: [
+        {
+            name: -,
+            famous: "can be a longer string",
+            birth:-,
+            death:-,
+            celebrityImage:-,
+        },
+        {
+            name: -,
+            famous: "can be a longer string",
+            birth:-,
+            death:-,
+            celebrityImage:-,
+        },
+        {
+            name: -,
+            famous: "can be a longer string",
+            birth:-,
+            death:-,
+            celebrityImage:-,
+        },
+    
+    
+    ]
+}
